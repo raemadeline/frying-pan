7626ece4f21a016d8eb55e4b27e478f88d06286e
raemadeline <maddie.horowitz@addepar.com>
2017-02-25 16:22:27 -0500

    visualization

--- scripts/api-connect.js
+++ scripts/api-connect.js
@@ -1,4 +1,6 @@
 var api_url = 'localhost:5000';
+var time = 0;
+var meditating = false;
 
 function shock () {
   $.ajax({
@@ -17,6 +19,34 @@ function shock () {
   });
 }
 
-$("#test").click(function(){
-  shock();
+function secondsToTime(s) {
+    var m = Math.floor(s/60); //Get remaining minutes
+    s -= m*60;
+    return (m < 10 ? '0'+m : m)+":"+(s < 10 ? '0'+s : s); //zero padding on minutes and seconds
+}
+
+$(".toggle-meditation").click(function() {
+  $('.visualization').toggleClass('meditating');
+  if (meditating == false) {
+    // reset time
+    time = 0;
+
+    // begin counter
+    interval = setInterval(function() {
+      time++;
+      $('.time').html(secondsToTime(time));
+    }, 1000);
+
+    $('.visualization-message').html('Stay relaxed or else');
+    $('.toggle-meditation').html('End Session');
+
+    meditating = true;
+  }
+  else {
+    clearInterval(interval);
+    $('.visualization-message').html('');
+    $('.toggle-meditation').html('Begin Meditation');
+
+    meditating = false;
+  }
 });
